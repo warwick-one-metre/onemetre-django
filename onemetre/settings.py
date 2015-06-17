7b69f92c14f542f9c32540a0621b620c0169fb31
Paul Chote <pchote@users.noreply.github.com>
2015-06-17 13:16:02 +0100

    Update settings for postgresql database.

--- onemetre/settings.py
+++ onemetre/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'onemetre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'onemetre',
+        'USER': 'django',
+        'PASSWORD': '', # Not actually needed, since we have configured postgres to accept all connections from a local socket
+        'HOST': '',
+        'PORT': '',
     }
 }
 
