fffd5c347691f7ed484be4b38279a5b3b51c1ef9
Paul Chote <p.chote@warwick.ac.uk>
2015-07-09 16:58:14 +0100

    Implement admin for powermeasurement.

--- power/admin.py
+++ power/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import PowerMeasurement
 
-# Register your models here.
+class PowerMeasurementAdmin(admin.ModelAdmin):
+	list_display = ('time', 'main_ups_status', 'main_ups_battery', 'main_ups_load', 'dome_ups_status', 'dome_ups_battery', 'dome_ups_load')
+
+admin.site.register(PowerMeasurement, PowerMeasurementAdmin)
